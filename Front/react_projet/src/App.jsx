e17503c2de26cd098c009eefa37faa56adbe7898
NicolasBELLANGER <nicobell93220@gmail.com>
2024-11-20 11:34:38 +0100

    Fourth Commit

--- Front/react_projet/src/App.jsx
+++ Front/react_projet/src/App.jsx
@@ -2,41 +2,54 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Bonjour from "./components/Bonjour.jsx";
+import MultipleFive from "./components/MultipleFive.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
+  const [name, setName] = useState('')
 
-  const [value, setValue] = useState("");
-    function handleChange(e) {
-        setValue(e.target.value);
-    }
+    const books = [
+        {
+            id: 1,
+            label: "book1"
+        },
+        {
+            id: 2,
+            label: "book1"
+        },
+        {
+            id: 3,
+            label: "book1"
+        },
+        {
+            id: 4,
+            label: "book1"
+        },
+        {
+            id: 5,
+            label: "book1"
+        }
+    ]
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
-      <div className="card">
+        <Bonjour name={name} setName={setName}/>
+        <MultipleFive />
+
+        <ul>
+            {books.map(book => (
+                <li key={book.id}>{book.label}</li>
+            ))}
+        </ul>
+
+        <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <br/>
-        <input value={value} onChange={handleChange}/>
-        <br/>
-        <div>{value}</div>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
