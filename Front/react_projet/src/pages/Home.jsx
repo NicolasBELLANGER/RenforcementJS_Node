f7af7897837e20f15ef44c9b298a84aec1cacb78
NicolasBELLANGER <nicobell93220@gmail.com>
2024-11-20 14:31:09 +0100

    Fourth Commit

---
+++ Front/react_projet/src/pages/Home.jsx
@@ -0,0 +1,36 @@
+import {useState, useEffect} from "react";
+
+function Home() {
+    const [books, setBooks] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://renforcementjs-node.onrender.com/api/books');
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                const data = await response.json();
+                setBooks(data);
+            } catch (error) {
+                console.error('Failed to fetch books:', error);
+            }
+        };
+
+        fetchData()
+    }, [] )
+
+    return (
+        <div>
+            <h1>Accueil</h1>
+            <p>test</p>
+            <ul>
+                {books.map(book => (
+                    <li key={book._id}> {book.label}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default Home;
